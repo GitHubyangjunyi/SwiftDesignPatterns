a80546a35cabe8efb1cd828830b13da17420fbac
杨俊艺 <32793979+GitHubyangjunyi@users.noreply.github.com>
2021-02-20 21:35:03 +0800

    最初版本

---
+++ SwiftDesignPatterns/View/ProductTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductTableCell.swift
+//  SwiftDesignPatterns
+//
+//  Created by 杨俊艺 on 2021/2/20.
+//
+
+import UIKit
+
+class ProductTableCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var stockStepper: UIStepper!
+    @IBOutlet weak var stockField: UITextField!
+    
+    var productId: Int?
+    
+}
--- SwiftDesignPatterns/ViewController.swift
+++ SwiftDesignPatterns/ViewController.swift
@@ -9,11 +9,89 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var totalStockLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        displayStockTotal()
+        
     }
 
-
+    func displayStockTotal() {
+        let stockTotal = products.reduce(0) { (result, item) -> Int in
+            result + item.4
+        }
+        totalStockLabel.text = "\(stockTotal) Products in Stock"
+    }
+    
+    @IBAction func stockLevelDidChange(_ sender: Any) {
+        if var currentCell = sender as? UIView {
+            while true {
+                currentCell = currentCell.superview!
+                if let cell = currentCell as? ProductTableCell {
+                    if let id = cell.productId {
+                        // 获得Cell在编辑的数据模型id
+                        var newStockLevel: Int?
+                        if let stepper = sender as? UIStepper {
+                            newStockLevel = Int(stepper.value)
+                        } else if let textfield = sender as? UITextField{
+                            if let newValue = Int(textfield.text ?? "") {
+                                newStockLevel = newValue
+                            }
+                        }
+                        // 数据格式合理
+                        if let level = newStockLevel {
+                            products[id].4 = level
+                            cell.stockStepper.value = Double(level)
+                            cell.stockField.text = String(level)
+                        }
+                    }
+                    break
+                }
+            }
+            displayStockTotal()
+        }
+    }
+    
+    // MARK: - 模型数据
+    var products = [
+        ("Kayak", "A boat for one person", "Watersports", 275.0, 10),
+        ("Lifejacket", "Protective and fashionable", "Watersports", 48.95, 14),
+        ("Soccer Ball", "FIFA-approved size and weight", "Soccer", 19.5, 32),
+        ("Corner Flags", "Give you playing field a professional touch", "Soccer", 34.95, 1),
+        ("Stadium", "Flat-packed 35,000-seat stadium", "Soccer", 79500.0, 4),
+        ("Thinking Cap", "Improve your brain efficiency by 75%", "Chess", 16.0, 8),
+        ("Unsteady Chair", "Secretly give your opponent a disadvantage", "Chess", 29.95, 3),
+        ("Human Chess Board", "A fun game for the family", "Chess", 75.0, 2),
+        ("Bling-Bling King", "Gold-plated, diamond-studded King", "Chess", 1200.0, 4)
+        ]
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell") as! ProductTableCell
+        let product = products[indexPath.row]
+        cell.nameLabel.text = product.0
+        cell.descriptionLabel.text = product.1
+        cell.stockStepper.value = Double(product.4)
+        cell.stockField.text = String(product.4)
+        cell.productId = indexPath.row
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
